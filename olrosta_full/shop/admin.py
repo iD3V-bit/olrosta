1f56bebf67f2b170193e8b611a2bdf160be444ce
DariusRakauskas <darius.rakauskas1@gmail.com>
2026-03-31 09:57:12 +0300

    Delete olrosta_full directory

--- olrosta_full/shop/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from .models import Category,Product,Project,ContactRequest
-
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Project)
-admin.site.register(ContactRequest)
